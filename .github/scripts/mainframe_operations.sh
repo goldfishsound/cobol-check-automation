bf35f231ad0071bbf8b3c485ffb14ff7b0bd4c6d
Thomas Martin <thomas@goldfishsound.com>
2025-09-26 10:20:46 +0200

    Added mainframe_operations.sh

--- .github/scripts/mainframe_operations.sh
+++ .github/scripts/mainframe_operations.sh
@@ -9,7 +9,7 @@ export PATH=$PATH:/usr/lpp/zowe/cli/node/bin
 # Check Java availability
 java -version
 # Set ZOWE_USERNAME
-ZOWE_USERNAME="Z99998" # Replace with the actual username
+ZOWE_USERNAME="Z75239" # Replace with the actual username
 # Change to the cobolcheck directory
 cd cobolcheck
 echo "Changed to $(pwd)"
