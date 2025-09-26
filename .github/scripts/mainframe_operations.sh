0f1c1e63f9001966d289b394221d6e81b3d7e2a2
Thomas Martin <thomas@goldfishsound.com>
2025-09-26 11:41:23 +0200

    Fixing naming error.

--- .github/scripts/mainframe_operations.sh
+++ .github/scripts/mainframe_operations.sh
@@ -6,31 +6,40 @@
 export PATH=$PATH:/usr/lpp/java/J8.0_64/bin
 export JAVA_HOME=/usr/lpp/java/J8.0_64
 export PATH=$PATH:/usr/lpp/zowe/cli/node/bin
+
 # Check Java availability
 java -version
+
 # Set ZOWE_USERNAME
 ZOWE_USERNAME="Z75239" # Replace with the actual username
-# Change to the cobolcheck directory
-cd cobolcheck
+
+# Change to the cobol-check directory
+cd cobol-check
 echo "Changed to $(pwd)"
 ls -al
-# Make cobolcheck executable
-chmod +x cobolcheck
-echo "Made cobolcheck executable"
+
+# Make cobol-check executable
+chmod +x cobol-check
+echo "Made cobol-check executable"
+
 # Make script in scripts directory executable
 cd scripts
 chmod +x linux_gnucobol_run_tests
 echo "Made linux_gnucobol_run_tests executable"
 cd ..
-# Function to run cobolcheck and copy files
+
+# Function to run cobol-check and copy files
 run_cobolcheck() {
 program=$1
 echo "Running cobolcheck for $program"
+
 # Run cobolcheck, but don't exit if it fails
 ./cobolcheck -p $program
 echo "Cobolcheck execution completed for $program (exceptions may have occurred)"
+
 # Check if CC##99.CBL was created, regardless of cobolcheck exit status
 if [ -f "CC##99.CBL" ]; then
+
 # Copy to the MVS dataset
 if cp CC##99.CBL "//'${ZOWE_USERNAME}.CBL($program)'"; then
 echo "Copied CC##99.CBL to ${ZOWE_USERNAME}.CBL($program)"
@@ -43,6 +52,7 @@ COBOL Programming
 Course #4 3.2.0|
 250217-2356
 fi
+
 # Copy the JCL file if it exists
 if [ -f "${program}.JCL" ]; then
 if cp ${program}.JCL "//'${ZOWE_USERNAME}.JCL($program)'"; then
@@ -54,6 +64,7 @@ else
 echo "${program}.JCL not found"
 fi
 }
+
 # Run for each program
 for program in NUMBERS EMPPAY DEPTPAY; do
 run_cobolcheck $program
